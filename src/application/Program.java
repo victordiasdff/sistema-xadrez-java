beca8905dd69cd361ebc758b6a7cc453cdf17eb3
Victor Dias <victordiasdff@gmail.com>
2023-02-24 16:54:41 -0300

    Primeira classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
