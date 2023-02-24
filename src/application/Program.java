97d1c0e10004373fc6824bf45c95cd9e90052bc3
Victor Dias <victordiasdff@gmail.com>
2023-02-24 17:11:14 -0300

    Implementacao de tabuleiro e pecas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
-import boardgame.Posicao;
+import boardgame.Tabuleiro;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos);
+		Tabuleiro tabuleiro = new Tabuleiro(8,8);
 	}
-
+	
 }
